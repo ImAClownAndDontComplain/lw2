4c96090d319f932fa8965e39c1c95590c12d09d5
ImAClownAndDontComplain <lukporey1317@bk.ru>
2022-04-09 17:26:46 +0500

    ??

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -9,8 +9,28 @@ using namespace glm;
 float scalex = 0.001;
 float scaley = 0.001;
 float scaler = 0.005;
-mat4 World;                   //translation
-mat4 Worldr;                  //rotation
+float scale = 3.14;
+
+mat4 World{ //translation
+    World[0][0] = 1.0f, World[0][1] = 0.0f, World[0][2] = 0.0f, World[0][3] = sinf(scalex),
+    World[1][0] = 0.0f, World[1][1] = 1.0f, World[1][2] = 0.0f, World[1][3] = sinf(scaley),
+    World[2][0] = 0.0f, World[2][1] = 0.0f, World[2][2] = 1.0f, World[2][3] = 0.0f,
+    World[3][0] = 0.0f, World[3][1] = 0.0f, World[3][2] = 0.0f, World[3][3] = 1.0f
+};
+
+mat4 Worldr{ //rotation
+    Worldr[0][0] = cosf(scaler), Worldr[0][1] = -sinf(scaler), Worldr[0][2] = 0.0f, Worldr[0][3] = 0.0,
+    Worldr[1][0] = sinf(scaler), Worldr[1][1] = cosf(scaler), Worldr[1][2] = 0.0f, Worldr[1][3] = 0.0,
+    Worldr[2][0] = 0.0f, Worldr[2][1] = 0.0f, Worldr[2][2] = 1.0f, Worldr[2][3] = 0.0f,
+    Worldr[3][0] = 0.0f, Worldr[3][1] = 0.0f, Worldr[3][2] = 0.0f, Worldr[3][3] = 1.0f
+};
+
+mat4 Worlds{ //scaling
+    Worlds[0][0] = 1+sinf(scale), Worlds[0][1] = 0.0f, Worlds[0][2] = 0.0f, Worlds[0][3] = 0.0,
+    Worlds[1][0] = 0.0f, Worlds[1][1] = 1+sinf(scale), Worlds[1][2] = 0.0f, Worlds[1][3] = 0.0,
+    Worlds[2][0] = 0.0f, Worlds[2][1] = 0.0f, Worlds[2][2] = 1+sinf(scale), Worlds[2][3] = 0.0f,
+    Worlds[3][0] = 0.0f, Worlds[3][1] = 0.0f, Worlds[3][2] = 0.0f, Worlds[3][3] = 1.0f
+};
 
 vec3 Triangle[3] = {
 { -0.1, -0.1, 0.0f },
@@ -28,23 +48,19 @@ vec4 mult(mat4 M, vec4 V) {
 }
 
 void trans(vec3 &V) {
-    /*if (V.x > 1)scalex *= -1;
-    if (V.y > 1)scaley *= -1;*/
-    World[0][0] = 1.0f; World[0][1] = 0.0f; World[0][2] = 0.0f; World[0][3] = 10*sinf(scalex);
-    World[1][0] = 0.0f; World[1][1] = 1.0f; World[1][2] = 0.0f; World[1][3] = 10 * sinf(scaley);
-    World[2][0] = 0.0f; World[2][1] = 0.0f; World[2][2] = 1.0f; World[2][3] = 0.0f;
-    World[3][0] = 0.0f; World[3][1] = 0.0f; World[3][2] = 0.0f; World[3][3] = 1.0f;
-    vec4 v1 = mult(World, vec4(V, 0.1));
-    std::cout << v1.x << "\n";
+    vec4 v1 = mult(World, vec4(V, 1.0));
+    std::cout << scaley << "\n";
     V = vec3(v1);
 }
 
 void rot(vec3& V) {
-    Worldr[0][0] = cosf(scaler); Worldr[0][1] = -sinf(scaler); Worldr[0][2] = 0.0f; Worldr[0][3] = 0.0;
-    Worldr[1][0] = sinf(scaler); Worldr[1][1] = cosf(scaler); Worldr[1][2] = 0.0f; Worldr[1][3] = 0.0;
-    Worldr[2][0] = 0.0f; Worldr[2][1] = 0.0f; Worldr[2][2] = 1.0f; Worldr[2][3] = 0.0f;
-    Worldr[3][0] = 0.0f; Worldr[3][1] = 0.0f; Worldr[3][2] = 0.0f; Worldr[3][3] = 1.0f;
-    vec4 v1 = mult(Worldr, vec4(V, 0.1));
+    vec4 v1 = mult(Worldr, vec4(V, 1.0));
+    //std::cout << v1.x << "\n";
+    V = vec3(v1);
+}
+
+void scal(vec3& V) {
+    vec4 v1 = mult(Worlds, vec4(V, 1.0));
     std::cout << v1.x << "\n";
     V = vec3(v1);
 }
@@ -53,12 +69,13 @@ GLuint VBO;
 
 void RenderSceneGB() { 
     for (int i = 0; i < 3; i++) {
-        if (i == 0 && Triangle[i].x < -1)scalex *= -1;
-        if (i == 0 && Triangle[i].x > 1)scalex *= -1;
-        if (i == 0 && Triangle[i].y > 1)scaley *= -1;
-        if (i == 0 && Triangle[i].y < -1)scaley *= -1;
+        if (Triangle[i].x < -1) scalex *= -1; 
+        if (Triangle[i].x > 1) scalex *= -1; 
+        if (Triangle[i].y > 1) scaley *= -1; 
+        if (i==0 && Triangle[i].y < -1) scaley *= -1;
         trans(Triangle[i]);
-        rot(Triangle[i]);
+        //rot(Triangle[i]);
+        //scal(Triangle[i]);
     }
     glGenBuffers(1, &VBO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
